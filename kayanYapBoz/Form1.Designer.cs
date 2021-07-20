42654e0c647f8c35101a68c24a8879ca2a9fe2c6
EmineSARI <39420397+EmineSARI@users.noreply.github.com>
2021-07-20 12:43:04 +0300

    projenin son hali

--- kayanYapBoz/Form1.Designer.cs
+++ kayanYapBoz/Form1.Designer.cs
@@ -28,13 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb0 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnKaristir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb0
+            // 
+            this.pb0.Location = new System.Drawing.Point(347, 42);
+            this.pb0.Margin = new System.Windows.Forms.Padding(1);
+            this.pb0.Name = "pb0";
+            this.pb0.Size = new System.Drawing.Size(100, 96);
+            this.pb0.TabIndex = 0;
+            this.pb0.TabStop = false;
+            this.pb0.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(449, 42);
+            this.pb1.Margin = new System.Windows.Forms.Padding(1);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(100, 96);
+            this.pb1.TabIndex = 1;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(551, 42);
+            this.pb2.Margin = new System.Windows.Forms.Padding(1);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(100, 96);
+            this.pb2.TabIndex = 2;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(347, 140);
+            this.pb3.Margin = new System.Windows.Forms.Padding(1);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(100, 96);
+            this.pb3.TabIndex = 3;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(449, 140);
+            this.pb4.Margin = new System.Windows.Forms.Padding(1);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(100, 96);
+            this.pb4.TabIndex = 4;
+            this.pb4.TabStop = false;
+            this.pb4.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(551, 140);
+            this.pb5.Margin = new System.Windows.Forms.Padding(1);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(100, 96);
+            this.pb5.TabIndex = 5;
+            this.pb5.TabStop = false;
+            this.pb5.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(347, 238);
+            this.pb6.Margin = new System.Windows.Forms.Padding(1);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(100, 96);
+            this.pb6.TabIndex = 6;
+            this.pb6.TabStop = false;
+            this.pb6.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(449, 238);
+            this.pb7.Margin = new System.Windows.Forms.Padding(1);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(100, 96);
+            this.pb7.TabIndex = 7;
+            this.pb7.TabStop = false;
+            this.pb7.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(551, 238);
+            this.pb8.Margin = new System.Windows.Forms.Padding(1);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(100, 96);
+            this.pb8.TabIndex = 8;
+            this.pb8.TabStop = false;
+            this.pb8.Click += new System.EventHandler(this.pb0_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
+            this.pictureBox10.Location = new System.Drawing.Point(62, 15);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(179, 243);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.pictureBox10_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnKaristir
+            // 
+            this.btnKaristir.Location = new System.Drawing.Point(121, 281);
+            this.btnKaristir.Name = "btnKaristir";
+            this.btnKaristir.Size = new System.Drawing.Size(75, 23);
+            this.btnKaristir.TabIndex = 10;
+            this.btnKaristir.Text = "Karıştır";
+            this.btnKaristir.UseVisualStyleBackColor = true;
+            this.btnKaristir.Click += new System.EventHandler(this.btnKaristir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKaristir);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.pb0);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pb0;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnKaristir;
     }
 }
 
--- kayanYapBoz/Form1.cs
+++ kayanYapBoz/Form1.cs
@@ -12,9 +12,93 @@ namespace kayanYapBoz
 {
     public partial class Form1 : Form
     {
+        PictureBox[] Kutular;
+        Point [] dogruKoordinatlar= new Point[9];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Kutular = new PictureBox[] {pb0,pb1,pb2,pb3,pb4,pb5,pb6,pb7,pb8 };
+            for (int i=0;i<9;i++)
+            {
+                dogruKoordinatlar[i] = Kutular[i].Location;
+            }
+            resmiParcala();
+
+        }
+
+        private void resmiParcala()
+        {
+            Bitmap resim = new Bitmap(pictureBox10.Image,450,450);
+
+            for (int i=0;i<3;i++)
+            {
+                for (int j=0;j<3;j++)
+                {
+                    int indeks = i * 3 + j;
+
+                    Bitmap parca = resim.Clone(new Rectangle(j * 150, j * 150, 150, 150), resim.PixelFormat);
+                    Graphics cizer = Graphics.FromImage(parca);
+                    cizer.DrawString(indeks.ToString(), Font, Brushes.Red, 15, 15);
+
+                    Kutular[indeks].Image = parca;
+
+
+                }
+
+            }
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox10.Image = Image.FromFile(openFileDialog1.FileName);
+                resmiParcala();
+            }
+        }
+
+        private void btnKaristir_Click(object sender, EventArgs e)
+        {
+            pb8.Visible = false;
+
+            Random rastgele = new Random();
+            for(int i = 0; i < 1000; i++)
+            {
+                int kutuNo = rastgele.Next(8);
+                oynat(Kutular[kutuNo]);
+            }
+
+        }
+
+        private void pb0_Click(object sender, EventArgs e)
+        {
+            oynat((PictureBox)sender);
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (Kutular[i].Location != dogruKoordinatlar[i])
+                    return;
+            }
+            pb8.Visible = true;
+            MessageBox.Show("Tebrikler");
+        }
+
+        private void oynat(PictureBox kutu)
+        {
+            if(kutu.Left +102 == pb8.Left && kutu.Top == pb8.Top ||
+               kutu.Left - 102 == pb8.Left && kutu.Top == pb8.Top ||
+               kutu.Left == pb8.Left && kutu.Top - 102 == pb8.Top ||
+               kutu.Left  == pb8.Left && kutu.Top +102== pb8.Top)
+            {
+                Point gecici = kutu.Location;
+                kutu.Location = pb8.Location;
+                pb8.Location = gecici;
+            }
         }
     }
 }
